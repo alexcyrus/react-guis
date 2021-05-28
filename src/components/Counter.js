491797237a310a1c692f60eefef50265f034eebf
Alex Cyrus <alex@alexcyrus.com>
2021-05-28 13:08:56 -0400

    implement counter

---
+++ src/components/Counter.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    }
+  }
+
+  increase = () => {
+    this.setState({
+      count: this.state.count + 1
+    })
+  };
+
+  render() {
+    return (
+      <div>
+        <p>{this.state.count}</p>
+        <button onClick={this.increase}>Count</button>
+      </div>
+    );
+  }
+}
+
+export default Counter;

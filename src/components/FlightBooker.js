82a4e43b45cf9ac3a33c1f568d3a77b81183df6d
Alex Cyrus <alex@alexcyrus.com>
2021-05-29 19:07:09 -0400

    implement flight booker

--- src/components/FlightBooker.js
+++ src/components/FlightBooker.js
@@ -1,15 +1,32 @@
 import React, { useState } from 'react';
+import Dropdown from 'react-dropdown';
+import 'react-dropdown/style.css';
 import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
 
 function FlightBooker() {
+  const options = [
+    'one-way flight', 'return flight'
+  ];
+  const defaultOption = options[0];
   const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
 
   return (
-    <DatePicker
-      selected = {startDate}
-      onChange = {date => setStartDate(date)}
-    />
+    <div>
+      <Dropdown options={options} value={defaultOption} placeholder="Select an option" />
+      <DatePicker
+        selected = {startDate}
+        onChange = {date => setStartDate(date)}
+        minDate = {new Date()}
+      />
+      <DatePicker
+        selected = {endDate}
+        onChange = {date => setEndDate(date)}
+        minDate = {new Date()}
+      />
+      <button>Book</button>
+    </div>
   );
 }
 

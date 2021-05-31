b3117660c4e4b5cd34dd7fd37ae2e80e42e036da
Alex Cyrus <alex@alexcyrus.com>
2021-05-31 17:28:07 -0400

    update flight booker

--- src/components/FlightBooker.js
+++ src/components/FlightBooker.js
@@ -4,14 +4,33 @@ import 'react-dropdown/style.css';
 import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
 import Button from 'react-bootstrap/Button';
+import moment from 'moment'
 
 function FlightBooker() {
   const options = [
     'one-way flight', 'return flight'
   ];
+
   const defaultOption = options[0];
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
+
+  const [status, setStatus] = useState("one-way flight")
+
+  const handleSetStatus = (values) => {
+    setStatus(values.value)
+  }
+
+  const handleAlert = () => {
+   const formattedStartDate =  moment(startDate).format('MMMM Do YYYY');
+   const formattedEndDate =  moment(endDate).format('MMMM Do YYYY');
+    if(status === "one-way flight"){
+      alert(`You have booked a one-way flight on ${formattedStartDate}.`)
+    }else{
+      alert(`You have booked a round-trip flight from ${formattedStartDate} to ${formattedEndDate}.`)
+    }
+  
+  }
 
   return (
     <div>
@@ -19,18 +38,19 @@ function FlightBooker() {
         options={options}
         value={defaultOption}
         placeholder="Select an option"
+        onChange={(values) => {handleSetStatus(values)}}
       />
       <DatePicker
         selected = {startDate}
         onChange = {date => setStartDate(date)}
         minDate = {new Date()}
       />
-      <DatePicker
+     {status === "return flight" ?  <DatePicker
         selected = {endDate}
         onChange = {date => setEndDate(date)}
         minDate = {new Date()}
-      />
-      <Button>Book</Button>
+      /> : "" }
+      <Button onClick={() => {handleAlert()}}>Book</Button>
     </div>
   );
 }
